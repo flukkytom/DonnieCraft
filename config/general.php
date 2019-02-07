936fecaa6a79b91f098c2d79400cf309f26a2bd2
Michael <flukkytom@gmail.com>
2019-02-07 10:23:11 -0700

    new origin

--- config/general.php
+++ config/general.php
@@ -36,7 +36,7 @@ return [
         'siteUrl' => null,
 
         // Dev Mode (see https://craftcms.com/support/dev-mode)
-        'devMode' => true,
+        'devMode' => false,
     ],
 
     // Staging environment settings
